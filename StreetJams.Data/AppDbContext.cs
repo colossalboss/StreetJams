3dd9f61aa09317c18c950d1ee9a61b4e3378e5d3
Ogbara Gerrald Godstar <sengodstar@gmail.com>
2020-05-20 20:17:08 +0100

    Initialize project

---
+++ StreetJams.Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using StreetJams.Entities;
+
+namespace StreetJams.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
---
+++ StreetJams.Entities/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace StreetJams.Entities
+{
+    public class Song
+    {
+        public Guid Id { get; set; }
+
+        [Display(Name = "Song Name")]
+        [NotMappedAttribute]
+        public IFormFile SongName { get; set; }
+    }
+}
